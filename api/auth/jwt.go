d656aad1c34ffb76ebd7985403a4dbb338136c02
qburst-praven <8088185+qburst-praven@users.noreply.github.com>
2025-06-23 16:59:56 +0530

    fixing file formats as suggested (#19)

--- api/auth/jwt.go
+++ api/auth/jwt.go
@@ -9,32 +9,21 @@ import (
 var jwtSecretKey = []byte("your-super-secret-and-long-enough-key-please-change-this-ASAP")
 
 // GenerateJWT creates a new JWT for a given username.
-
 func GenerateJWT(username string) (string, time.Time, error) {
-
 	expirationTime := time.Now().Add(24 * time.Hour) // Token valid for 24 hours
 
 	claims := &jwt.RegisteredClaims{
-
-		Subject: username,
-
+		Subject:   username,
 		ExpiresAt: jwt.NewNumericDate(expirationTime),
-
-		IssuedAt: jwt.NewNumericDate(time.Now()),
-
-		Issuer: "finala-api",
+		IssuedAt:  jwt.NewNumericDate(time.Now()),
+		Issuer:    "finala-api",
 	}
 
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
-
 	tokenString, err := token.SignedString(jwtSecretKey)
-
 	if err != nil {
-
 		return "", time.Time{}, err
-
 	}
 
 	return tokenString, expirationTime, nil
-
 }
